27f9c1c410eff1b8c0b2a9c3b18d2cc5c3634731
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2023-01-12 15:33:28 -0500

    add CacheT and MonadCache

--- src/Control/Monad/Cache/CacheT.hs
+++ src/Control/Monad/Cache/CacheT.hs
@@ -1,1 +1,48 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Control.Monad.Cache.CacheT where
+
+import Control.Monad.Cache.Class.MonadCache (MonadCache, Key, Value, cache)
+import Control.Monad.State (MonadTrans, StateT(..), evalStateT, execStateT, lift, runStateT)
+import Data.Hashable (Hashable)
+import Data.Cache (Cache)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Class (MonadTrans)
+
+
+newtype CacheT k v m a = CacheT (StateT (Cache k v) m a)
+deriving instance Hashable k => MonadTrans (CacheT k v)
+
+instance (Applicative m, Hashable k) => Functor (CacheT k v m) where
+    fmap f (CacheT x) = CacheT (fmap f x)
+
+instance (Hashable k, Monad m) => Applicative (CacheT k v m) where
+    pure x = CacheT $ StateT (\s -> pure (x, s))
+    (CacheT (StateT mf)) <*> (CacheT (StateT mx)) = CacheT . StateT $ \ s -> do
+        (f, s') <- mf s
+        (x, s'') <- mx s'
+        pure (f x, s'')
+
+instance (Hashable k, Monad m) => Monad (CacheT k v m) where
+    (CacheT (StateT x)) >>= f = CacheT . StateT $ \s ->
+        x s >>= \(v, s') -> case f v of CacheT (StateT st) -> st s'
+
+instance (Hashable k, Monad m) => MonadCache (CacheT k v m) where
+    type instance Key (CacheT k v m) = k
+    type instance Value (CacheT k v m) = v
+    cache f = CacheT (StateT $ pure . f)
+
+instance (Hashable k, MonadIO m) => MonadIO (CacheT k v m) where
+    liftIO = lift . liftIO
+
+runCacheT :: Monad m => CacheT k v m a -> Cache k v -> m (a, Cache k v)
+runCacheT (CacheT x) = runStateT x
+
+evalCacheT :: Monad m => CacheT k v m a -> Cache k v -> m a
+evalCacheT (CacheT x) = evalStateT x
+
+execCacheT :: Monad m => CacheT k v m a -> Cache k v -> m (Cache k v)
+execCacheT (CacheT x) = execStateT x
--- src/Control/Monad/Cache/Class/MonadCache.hs
+++ src/Control/Monad/Cache/Class/MonadCache.hs
@@ -1,2 +1,46 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Control.Monad.Cache.Class.MonadCache where
     
+import Prelude
+
+import           Data.Hashable (Hashable)
+import           Data.Maybe (Maybe(..))
+import           Data.Cache (Cache)
+import qualified Data.Cache as Cache
+import           Data.Functor (($>))
+
+
+class (Hashable (Key m), Monad m) => MonadCache m where
+    type Key m
+    type Value m
+    cache :: (Cache (Key m) (Value m) -> (a, Cache (Key m) (Value m))) -> m a
+
+insert :: MonadCache m => Key m -> Value m -> m ()
+insert k v = cache (\c -> ((), Cache.insert k v c))
+
+-- TODO should I rename this `lookup`?
+-- | retrieve a value from the cache by its key 
+retrieve :: MonadCache m => Key m -> m (Maybe (Value m))
+retrieve k = cache (Cache.lookup k)
+
+-- | retrieve a value that depends on the entire cache
+retrieves :: MonadCache m => (Cache (Key m) (Value m) -> a) -> m a
+retrieves f = cache (\c -> (f c, c))
+
+evict :: MonadCache m => Key m -> m ()
+evict k = cache (\c -> ((), Cache.evict k c ))
+
+evictAll :: MonadCache m => m ()
+evictAll = cache (\c -> ((), Cache.evictAll c))
+
+-- | gets the value using the cache if possible,
+-- | otherwise it runs the effect and puts it in the cache
+fetch :: MonadCache m => (Key m -> m (Value m)) -> Key m -> m (Value m)
+fetch f k = retrieve k >>= (\x -> case x of
+    Nothing -> f k >>= \v -> insert k v $> v
+    Just v -> pure v)
+
+size :: MonadCache m => m Int
+size = retrieves Cache.size

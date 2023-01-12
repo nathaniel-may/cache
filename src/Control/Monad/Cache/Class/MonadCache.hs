add27802ecdd05c4cd86cfb72176cdf606ea818f
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2023-01-12 17:48:32 -0500

    rename retrieve

--- src/Control/Monad/Cache/Class/MonadCache.hs
+++ src/Control/Monad/Cache/Class/MonadCache.hs
@@ -3,7 +3,7 @@
 
 module Control.Monad.Cache.Class.MonadCache where
     
-import Prelude
+import Prelude hiding (lookup)
 
 import           Data.Hashable (Hashable)
 import           Data.Maybe (Maybe(..))
@@ -20,14 +20,13 @@ class (Hashable (Key m), Monad m) => MonadCache m where
 insert :: MonadCache m => Key m -> Value m -> m ()
 insert k v = cache (\c -> ((), Cache.insert k v c))
 
--- TODO should I rename this `lookup`?
--- | retrieve a value from the cache by its key 
-retrieve :: MonadCache m => Key m -> m (Maybe (Value m))
-retrieve k = cache (Cache.lookup k)
+-- | lookup a value from the cache by its key 
+lookup :: MonadCache m => Key m -> m (Maybe (Value m))
+lookup k = cache (Cache.lookup k)
 
--- | retrieve a value that depends on the entire cache
-retrieves :: MonadCache m => (Cache (Key m) (Value m) -> a) -> m a
-retrieves f = cache (\c -> (f c, c))
+-- | lookup a value that depends on the entire cache
+fromCache :: MonadCache m => (Cache (Key m) (Value m) -> a) -> m a
+fromCache f = cache (\c -> (f c, c))
 
 evict :: MonadCache m => Key m -> m ()
 evict k = cache (\c -> ((), Cache.evict k c ))
@@ -38,9 +37,9 @@ evictAll = cache (\c -> ((), Cache.evictAll c))
 -- | gets the value using the cache if possible,
 -- | otherwise it runs the effect and puts it in the cache
 fetch :: MonadCache m => (Key m -> m (Value m)) -> Key m -> m (Value m)
-fetch f k = retrieve k >>= (\x -> case x of
+fetch f k = lookup k >>= (\x -> case x of
     Nothing -> f k >>= \v -> insert k v $> v
     Just v -> pure v)
 
 size :: MonadCache m => m Int
-size = retrieves Cache.size
+size = fromCache Cache.size
